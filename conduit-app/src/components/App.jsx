5a67b4cb9852c6edf312e8bb4e1de5b95a66e4b5
ravindrapv <ravindra.ravi4849@gmail.com>
2022-12-03 21:36:37 +0530

    Day-1 of blog-app-ui

---
+++ conduit-app/src/components/App.jsx
@@ -0,0 +1,20 @@
+import Heder from "./Heder";
+import Hero from "./Hero";
+import Home from "./Home";
+import Login from './Login'
+import Regisration from './Registreation'
+
+function App() {
+  return (
+    <>
+      <Heder />
+      <Hero />
+      <h1 className=" text-center">hello world</h1>
+      <Home />
+      <Login />
+      <Regisration />
+    </>
+  );
+}
+
+export default App;
---
+++ conduit-app/src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Hero() {
+    return (
+        <>
+            <section className="dark:bg-gray-800 dark:text-gray-100 bg-indigo-600">
+                <div className="container mx-auto flex flex-col items-center px-4 py-8 text-center md:py-16 md:px-10 lg:px-16 xl:max-w-2xl">
+                    <h1 className="text-4xl font-bold leading-none sm:text-5xl">conduit
+                        <span className="dark:text-violet-400">  A place to share your </span>knowledge.
+                    </h1>
+                    <p className="px-8 mt-8 mb-12 text-lg">Don't focus on having a great blog. Focus on producing a blog that's great for your readers!</p>
+                </div>
+            </section>
+        </>
+    );
+}
